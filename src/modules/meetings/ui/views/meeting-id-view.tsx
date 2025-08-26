99780f4767397e1b909c2ebf141288078a343b5e
ADITYA0018TH <aditya.raj862003@gmail.com>
2025-08-26 15:17:01 +0530

    21: meeting variants

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -9,8 +9,12 @@ import { useConfirm } from "@/hooks/use-confirm";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 
+import { ActiveState } from "../components/active-state";
+import { UpcomingState } from "../components/upcoming-state";
+import { CancelledState } from "../components/cancelled-state";
 import { MeetingIdViewHeader } from "../components/meeting-id-view-header";
 import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
+import { ProcessingState } from "../components/processing-state";
 
 interface Props {
     meetingId: string;
@@ -45,28 +49,48 @@ export const MeetingIdView = ({ meetingId }: Props) => {
     const handelRemoveMeeting = async () => {
         const ok = await confirmRemove();
 
-        if(!ok) return;
+        if (!ok) return;
 
         await removeMeeting.mutateAsync({ id: meetingId });
     };
 
+    const isActive = data.status === "active";
+    const isUpcoming = data.status === "upcoming";
+    const isCancelled = data.status === "cancelled";
+    const isCompleted = data.status === "completed";
+    const isProcessing = data.status === "processing";
+
     return (
         <>
-        <RemoveConfirmation />
-        <UpdateMeetingDialog
-        open={updateMeetingDialogOpen}
-        onOpenChange={setUpdateMeetingDialogOpen}
-        initialValues={data}
-        />
-        <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <MeetingIdViewHeader 
-            meetingId={meetingId}
-            meetingName={data.name}
-            onEdit={() => setUpdateMeetingDialogOpen(true)}
-            onRemove={handelRemoveMeeting}
+            <RemoveConfirmation />
+            <UpdateMeetingDialog
+                open={updateMeetingDialogOpen}
+                onOpenChange={setUpdateMeetingDialogOpen}
+                initialValues={data}
             />
-            {JSON.stringify(data, null, 2)}
-        </div>
+            <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
+                <MeetingIdViewHeader
+                    meetingId={meetingId}
+                    meetingName={data.name}
+                    onEdit={() => setUpdateMeetingDialogOpen(true)}
+                    onRemove={handelRemoveMeeting}
+                />
+                {isCancelled && <CancelledState />}
+                {isProcessing && <ProcessingState />}
+                {isCompleted && <div>Completed</div>}
+                {isActive && (
+                    <ActiveState
+                        meetingId={meetingId}
+                    />
+                )}
+                {isUpcoming && (
+                    <UpcomingState
+                        meetingId={meetingId}
+                        onCancelMeeting={() => { }}
+                        isCancelling={false}
+                    />
+                )}
+            </div>
         </>
     )
 };
